af71fcf08f44579565b788e8cd5a61611c817a20
Kaytii210 <Kaytii210@users.noreply.github.com>
2025-12-06 10:20:22 +0000

    deploy: Kaytii210/kaytii210-blog-source@6772736d7bfd364da284b0764d0fc203a544b815

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1764655664',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1765016419',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
